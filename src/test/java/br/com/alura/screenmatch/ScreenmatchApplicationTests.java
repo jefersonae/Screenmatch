0f9cced55d2fdc8615aaca73d3c9520aa4cc86ef
Jeferson de Souza Andrade <38109358+jefersonae@users.noreply.github.com>
2024-05-10 05:30:06 -0300

    initial project

--- src/test/java/br/com/alura/screenmatch/ScreenmatchApplicationTests.java
+++ src/test/java/br/com/alura/screenmatch/ScreenmatchApplicationTests.java
@@ -1,13 +1,18 @@
 package br.com.alura.screenmatch;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ScreenmatchApplicationTests {
+class ScreenmatchApplicationTests implements CommandLineRunner {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
